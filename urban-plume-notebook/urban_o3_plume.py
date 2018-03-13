92239bbea6564909f37741a914c26ec7d338dc8d
Dominik Brunner <dominik.brunner@empa.ch>
2018-03-13 22:02:04 +0100

    improved plot quality
    added legend, better colors
    improved printing of min/max values

--- urban-plume-notebook/urban_o3_plume.py
+++ urban-plume-notebook/urban_o3_plume.py
@@ -43,50 +43,64 @@ def urban_plume(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,
                             CO3=CO3,T=T)
     
     colors = {}
-    colors['O3'] = 'blue'
-    colors['ROG'] = 'purple'
-    colors['NO'] = 'blue'
-    colors['NO2'] = 'darkblue'
-    colors['OH'] = 'grey'
-    colors['HO2'] = 'darkgrey'
-    colors['HNO3'] = 'green'
-    colors['H2O2'] = 'orange'
+    colors['O3'] = 'tab:red'
+    colors['ROG'] = 'tab:blue'
+    colors['NO'] = 'tab:blue'
+    colors['NO2'] = 'tab:cyan'
+    colors['OH'] = 'tab:olive'
+    colors['HO2'] = 'tab:green'
+    colors['HNO3'] = 'tab:pink'
+    colors['H2O2'] = 'tab:purple'
     
     if doPlot:        
-        fs = 14
+        fs = 14 # font size for axis titles and labels
+        lw = 2  # default line width
         xlim = (2.,175.)
         fig, (ax1, ax2) = plt.subplots(2)
         ax1.set_xlabel('Distance (km)', fontsize=fs)
         ax1.set_xlim(xlim)
         #ax1.set_ylim(0,160.)
-        ax1.set_ylabel('conc ($\mu$g m$^{-3}$)', fontsize=fs)
+        ax1.set_ylabel('O$_3$, ROG ($\mu$g m$^{-3}$)', fontsize=fs)
         ax1.plot(plume['dist'], plume['conc']['O3'],color=colors['O3'],
-                 linewidth=4)
-        ax1.plot(plume['dist'], plume['conc']['ROG'],color=colors['ROG'])
+                 linewidth=lw, label='O$_3$')
+        ax1.plot(plume['dist'], plume['conc']['ROG'],color=colors['ROG'],
+                 linewidth=lw, label = 'ROG')
         ax1.tick_params(axis='y')
+        ax1.legend(bbox_to_anchor=(0.03, 0.86, 0.3, .102), loc=3,
+                   ncol=2, mode="expand", borderaxespad=0., fontsize=fs)
         
         #ax2 = ax1.twinx()  # instantiate a second axis using the same x-axis
         color = 'red'
         ax2.set_xlabel('Distance (km)', fontsize=fs)
         ax2.set_xlim(xlim)
         #ax2.set_ylim(0,30.)
-        ax2.set_ylabel('conc ($\mu$g m$^{-3}$)', fontsize=fs)
-        ax2.plot(plume['dist'], plume['conc']['NO'], color=colors['NO'])
-        ax2.plot(plume['dist'], plume['conc']['NO2'], color=colors['NO2'])
-        ax2.plot(plume['dist'], plume['conc']['H2O2'], 
-                 color=colors['H2O2'], linewidth=4)
-        ax2.plot(plume['dist'], plume['conc']['HNO3'], 
-                 color=colors['HNO3'], linewidth=4)
+        ax2.set_ylabel('NO, NO$_2$, HNO$_3$, H$_2$O$_2$ ($\mu$g m$^{-3}$)', 
+                       fontsize=fs)
+        ax2.plot(plume['dist'], plume['conc']['NO'], color=colors['NO'],
+                 linewidth=lw, label='NO')
+        ax2.plot(plume['dist'], plume['conc']['NO2'], color=colors['NO2'],
+                 linewidth=lw, label='NO$_2$')
+        ax2.plot(plume['dist'], plume['conc']['H2O2'], color=colors['H2O2'],
+                 linewidth=lw, label='H$_2$O$_2$')
+        ax2.plot(plume['dist'], plume['conc']['HNO3'], color=colors['HNO3'],
+                 linewidth=lw, label='HNO$_3$')
         ax2.tick_params(axis='y')
+        ax2.legend(bbox_to_anchor=(0.03, 0.76, 0.3, .102), loc=3,
+                   ncol=2, mode="expand", borderaxespad=0., fontsize=fs)
         
         if showHOx:
             ax3 = ax1.twinx()  # instantiate a third axis using the same x-axis
             color = 'green'
-            ax3.set_ylim(0,0.2)
-            ax3.set_ylabel('conc ($\mu$g m$^{-3}$)', color=color, fontsize=fs)
-            ax3.plot(plume['dist'], plume['conc']['OH']*100,color=colors['OH'])
-            ax3.plot(plume['dist'], plume['conc']['HO2'], color=colors['HO2'])
+            #ax3.set_ylim(0,0.2)
+            ax3.set_ylabel('OH, HO$_2$ ($\mu$g m$^{-3}$)', color=color, fontsize=fs)
+            ax3.plot(plume['dist'], plume['conc']['OH']*100,
+                     color=colors['OH'],linewidth=lw, label='OH*100')
+            ax3.plot(plume['dist'], plume['conc']['HO2'], color=colors['HO2'],
+                     linewidth=lw, label='HO$_2$')
             ax3.tick_params(axis='y', labelcolor=color)
+            ax3.legend(bbox_to_anchor=(0.67, 0.86, 0.3, .102), loc=3,
+                       ncol=2, mode="expand", borderaxespad=0., fontsize=fs)
+
 
         plt.rcParams['figure.figsize']=(12,10)
         #fig.tight_layout()
@@ -97,12 +111,19 @@ def urban_plume(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,
             
         # print minimum and maximum concentrations
         species = ['O3','NO','NO2','ROG','HNO3','H2O2','HO2','OH']
+        sstr = "     "
+        minstr = "Min: "
+        maxstr = "Max: "
         for i in species:
-            print("Min %4s:  %8.3f,  Max %4s:  %8.3f" %
-                  (i,min(plume['conc'][i]),i,max(plume['conc'][i])))
+            sstr = sstr + "%8s" % i
+            minstr = minstr+"%8.3f" % min(plume['conc'][i])
+            maxstr = maxstr+"%8.3f" % max(plume['conc'][i])
+        print(sstr)
+        print(minstr)
+        print(maxstr)
         
 
-def simple_boxmodel(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,T=303.0):
+def simple_boxmodel(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=20.0,T=303.0):
     
     # define some constants
     
@@ -113,7 +134,6 @@ def simple_boxmodel(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,T=303.0):
     # reaction rates
     k1 = 15e-12
     k2 = 3.7e-12 * exp(240./T)
-    k10 = 0.0
     k3 = 1.8e-12 * exp(-1370./T)
     k4 = 0.007
     k5 = 6.7e-11 * (T/300.)**-0.6
@@ -121,6 +141,7 @@ def simple_boxmodel(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,T=303.0):
     k7 = 2.3e-13 * exp(600./T)
     k8 = 1.1e-14 * exp(-500./T)
     k9= 4.6e-11 * exp(600./T)
+    k10 = 0.0
     
     # total time (tt), time step (dt) and number of time steps (nt)
     tt = distance * 1000. / speed
@@ -139,10 +160,10 @@ def simple_boxmodel(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,T=303.0):
     eta = 1e-8 * 8.314 * T/(6.023*96000)
     
     # inital concentrations
-    c['OH'][0] =  0.0001 / eta
-    c['HO2'][0] = 0.1 / eta
-    c['NO'][0] = 0.8 * CNOX / eta
-    c['NO2'][0] = 0.2 * CNOX / eta
+    c['OH'][0] =  0.000 / eta
+    c['HO2'][0] = 0.0 / eta
+    c['NO'][0] = 0.4 * CNOX / eta
+    c['NO2'][0] = 0.6 * CNOX / eta
     c['O3'][0] = CO3 / eta
     c['ROG'][0] = CROG / eta
     c['HNO3'][0] = 0.
@@ -163,7 +184,8 @@ def simple_boxmodel(SNOX=0.01,SROG=0.08,CNOX=1.0,CROG=4.0,CO3=30.0,T=303.0):
             sROG = 0.
         
         d['OH'] = k1*c['ROG'][j]+k10+k5*c['NO2'][j]+k9*c['HO2'][j]
-        p['OH'] = k2*c['HO2'][j]*c['NO'][j]+2*k6*c['O3'][j]+k8*c['HO2'][j]*c['O3'][j]
+        p['OH'] = k2*c['HO2'][j]*c['NO'][j]+2*k6*c['O3'][j]
+        +k8*c['HO2'][j]*c['O3'][j]
         
         d['HO2'] = k2*c['NO'][j]+2*k7*c['HO2'][j]+k8*c['O3'][j]+k9*c['OH'][j]
         p['HO2'] = k1*c['ROG'][j]*c['OH'][j]+k10*c['OH'][j]
